058c3c2cd91859e0e77fbb04ecd1a3e428e88d80
Wellington Omori <wpomori@gmail.com>
2020-02-04 23:13:39 -0300

    Order posts by published date and set posts variable

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 
 
 def post_list(request):
-    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
     
